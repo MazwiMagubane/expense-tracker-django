730b519a740247745fbf0654e2bfba2cf9247476
MazwiMagubane <mazwimagubane@gmail.com>
2025-05-09 21:48:52 +0200

    Stlying and data visualisations

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import ExpenseForm
 from .models import Expense
+import datetime
+from django.db.models import Sum
 
 # Create your views here.
 def index(request):
@@ -9,9 +11,32 @@ def index(request):
         if expense.is_valid():
             expense.save()
 
-    expenses = Expense.objects.all()    
+    expenses = Expense.objects.all()
+    total_expenses = expenses.aggregate(Sum('amount'))
+
+    # Logic to calculate 365 days epenses
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = data.aggregate(Sum('amount'))
+    
+    # Logic to calculate 30 days epenses
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = data.aggregate(Sum('amount'))
+
+    # Logic to calculate 7 days epenses
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = data.aggregate(Sum('amount'))
+    
+    # Logic
+    daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
+
+    # Logic
+    categorical_sums = Expense.objects.filter().values('category').order_by('category').annotate(sum=Sum('amount'))
+
     expense_form = ExpenseForm()
-    return render(request, 'myapp/index.html', {'expense_form':expense_form, 'expenses':expenses})
+    return render(request, 'myapp/index.html', {'expense_form':expense_form, 'expenses':expenses, 'total_expenses':total_expenses, 'yearly_sum':yearly_sum, 'monthly_sum':monthly_sum, 'weekly_sum':weekly_sum, 'daily_sums':daily_sums, 'categorical_sums':categorical_sums})
 
 def edit(request, id):
     expense = Expense.objects.get(id=id)
@@ -25,3 +50,9 @@ def edit(request, id):
             return redirect('index')
         
     return render(request, 'myapp/edit.html', {'expense_form':expense_form})
+
+def delete(request, id):
+    if request.method == 'POST' and 'delete' in request.POST:
+        expense = Expense.objects.get(id=id)
+        expense.delete()
+    return redirect('index')
